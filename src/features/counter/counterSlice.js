a9b9292de67d5eef672819a046ba9c6d1367ceb2
saheb mohammadi <saheb.put92@gmail.com>
2021-11-16 17:40:48 +0330

    create counter slice

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: { value: 0 },
+  reducers: {
+    increment: (state, action) => {
+      state.value += action.payload;
+    },
+    decrement: (state, action) => {
+      state.value -= action.payload;
+    },
+  },
+});
+
+export const { decrement, increment } = counterSlice.actions;
+
+export default counterSlice.reducer;
