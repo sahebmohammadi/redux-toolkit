efd361cbd025d83ccc1a3ffb7992ee9a017bfdd9
saheb mohammadi <saheb.put92@gmail.com>
2021-11-16 17:11:30 +0330

    connet store to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-
