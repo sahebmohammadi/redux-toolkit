b3a624e1de49abd25d2cff52212927b14d91067a
saheb mohammadi <saheb.put92@gmail.com>
2021-11-16 21:03:38 +0330

    add todo-starter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import CounterComp from "./components/CounterComp";
-
+import AddTodoForm from "./components/Todos/AddTodoForm";
+import TodoList from "./components/Todos/TodoList";
+import TotalCompleteItems from "./components/Todos/TotalCompleteTodo";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <h1>hi saheb !</h1>
-      <CounterComp />
+    <div className="App container">
+      {/* <h1>hi saheb !</h1> */}
+      {/* <CounterComp /> */}
+      <AddTodoForm />
+      <TodoList />
+      <TotalCompleteItems />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { store } from "./features/store";
 import { Provider } from "react-redux";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 ReactDOM.render(
   <Provider store={store}>
     <App />
