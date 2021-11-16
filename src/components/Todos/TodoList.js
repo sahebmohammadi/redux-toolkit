54867bc545e67035557c76760e9fff79b8f847c2
saheb mohammadi <saheb.put92@gmail.com>
2021-11-16 21:33:54 +0330

    pulling todos from store

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,14 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  const todos = [
-    { id: 1, title: "todo1", completed: false },
-    { id: 2, title: "todo2", completed: false },
-    { id: 3, title: "todo3", completed: true },
-    { id: 4, title: "todo4", completed: false },
-    { id: 5, title: "todo5", completed: false },
-  ];
+  const todos = useSelector((state) => state.todos);
 
   return (
     <ul className="list-group">
