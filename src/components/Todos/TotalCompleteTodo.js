f0b00c9d9fde0c6e711dbb1cb8dfa9f544b4df47
saheb mohammadi <saheb.put92@gmail.com>
2021-11-16 23:12:53 +0330

    display nums of completed todos

--- src/components/Todos/TotalCompleteTodo.js
+++ src/components/Todos/TotalCompleteTodo.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const TotalCompleteItems = () => {
-  return <h4 className="mt-3">Total Complete Items: 5</h4>;
+  const { todos } = useSelector((state) => state.todos);
+  return (
+    <h4 className="mt-3">
+      Total Complete Items: {todos.filter((t) => t.completed).length}
+    </h4>
+  );
 };
 
 export default TotalCompleteItems;
